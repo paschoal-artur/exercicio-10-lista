8d43e9680fbd908c2050756e9b42fe2ff2c949a7
paschoal-artur <paschoal.artur@alu.ufc.br>
2023-04-02 00:11:18 -0300

    added code

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+#Receving value from user
+temp = float(input("What's the Celsius° temperature at your location? \n"))
 
+#Converting to fahrenheit
+fahr = float((temp * 1.8) + 32)
+
+#Printing result to user
+print(f"The temperature in Fahrenheit at you location is {fahr}°F")
